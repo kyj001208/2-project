2669257160fcd577bdc0a703f93e5ffe70a934bc
kyj <kyj001208@naver.com>
2024-08-09 10:29:37 +0900

    delete

--- src/main/java/com/green/petfirst/security/CustomOQuth2UserService.java
+++
@@ -1,5 +0,0 @@
-package com.green.petfirst.security;
-
-public class CustomOQuth2UserService {
-
-}
---
+++ src/main/java/com/green/petfirst/service/login/impl/SignupServiceProcess.java
@@ -0,0 +1,17 @@
+package com.green.petfirst.service.login.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.green.petfirst.domain.dto.login.SignSaveDTO;
+import com.green.petfirst.service.login.SignupService;
+
+@Service
+public class SignupServiceProcess implements SignupService {
+
+	@Override
+	public void save(SignSaveDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
