158a37429320aa229d802b6d6ae6acb3a22529b9
songol <taeju1119@gmail.com>
2024-08-13 13:08:03 +0900

    deliver

--- src/main/java/com/green/petfirst/domain/entity/ProductEntity.java
+++ src/main/java/com/green/petfirst/domain/entity/ProductEntity.java
@@ -15,7 +15,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @DynamicUpdate
 @Getter
 @Builder
--- src/main/java/com/green/petfirst/domain/repository/MemberRepository.java
+++ src/main/java/com/green/petfirst/domain/repository/MemberRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.green.petfirst.domain.entity.MemberEntity;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
 	Optional<MemberEntity> findByUserId(String userId);
