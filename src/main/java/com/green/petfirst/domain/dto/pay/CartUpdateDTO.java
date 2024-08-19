e4d96a4b9eae62734fe6f7a72c3b9b5b9dbbf1dc
kyj <kyj001208@naver.com>
2024-08-19 15:49:53 +0900

    feat cart updatecontroller

---
+++ src/main/java/com/green/petfirst/domain/dto/pay/CartUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.green.petfirst.domain.dto.pay;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartUpdateDTO {
+	
+	private int count;
+	//private long discountPrice;
+
+	
+}
