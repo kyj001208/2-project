1521a1b0f661f0458a9e18663c7f8759df8a3cdd
yhr <yhr0841@gmail.com>
2024-08-20 15:24:41 +0900

    myinfo

--- src/main/java/com/green/petfirst/domain/dto/login/MemberDTO.java
+++ src/main/java/com/green/petfirst/domain/dto/login/MemberDTO.java
@@ -9,11 +9,13 @@ import com.green.petfirst.security.Role;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
+@Getter
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor
 @Setter
 public class MemberDTO {
@@ -39,6 +41,7 @@ public class MemberDTO {
 	public MemberEntity toEntity(PasswordEncoder pass) {
 		
 		return MemberEntity.builder()
+				.memNo(this.id)
 				.userId(userId)
 				.password(pass.encode(password))
 				.name(name)
@@ -50,10 +53,5 @@ public class MemberDTO {
 				.roles(Set.of(Role.PETFIR))
 				.build();
 	}
-
-
-
-
-	
 
 }
